a20c92ce86b9c0b682ad0ebaf94ba7236b2b342e
Kelly Maass <kmaass77627@gmail.com>
2026-01-31 13:17:01 -0600

    setting up migrations and auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+const [user, setUser] = useState(null);
+
+// useEffect(() => {
+//   fetch("http://localhost:3000/me", {
+
+//   })
+//   .then((response) => {
+//      if (response.ok) {
+//        response.json()
+//     }
+//   })
+//    .then((json) => {
+//      console.log(json)
+//       setUser(json)
+//   })
+// }, [])
+
+function login(username, password) {
+  fetch("http://localhost:3000/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password })
+  }).then(res => {
+    if (res.ok) {
+      res.json().then(setUser);
+    }
+  });
+}
+
+
+function logout() {
+  fetch("http://localhost:3000/logout", {
+    method: "DELETE",
+  }).then(() => setUser(null));
+}
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> I am the App</h1>
+     
     </div>
   );
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :books
+  
+   post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+   post "/signup", to: "users#create"
+  get "/me", to: "users#show"
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
